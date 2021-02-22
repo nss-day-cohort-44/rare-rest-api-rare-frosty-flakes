c94a2f07a80a75bce6ff6020eaa8e2d0e9a94a73
Jayna Leitze <jayna.leitze@gmail.com>
2021-02-22 11:06:48 -0600

    add url router path

--- rare/urls.py
+++ rare/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
 from rareapi.views import register_user, login_user
+from rest_framework import routers
+from rareapi.views import Categories
 
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories',Categories, 'category')
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
