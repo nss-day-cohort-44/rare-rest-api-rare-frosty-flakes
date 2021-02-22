28785abc6e685ac80a2076605a128832605cdfd9
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-22 16:11:22 -0600

    Rewriting views/posts.py

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -25,86 +25,81 @@ class Posts(ViewSet):
         category = Category.objects.get(pk=request.data["categoryId"])
 
         post = Post()
-        post.user = scheduler
+        post.user = user
         post.category = category
         post.title = request.data["title"]
-        post.publication_date = request.data["publicationDate"]
+        post.publication_date = request.data["publication_date"]
         post.image_url = request.data["image_url"]
         post.content = request.data["content"]
-        post.approved = 
+        post.approved = request.data["approved"]
 
 
         try:
             post.save()
-            serializer = EventSerializer(event, context={'request': request})
+            serializer = PostSerializer(post, context={'request': request})
             return Response(serializer.data)
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request, pk=None):
-        """Handle GET requests for single event
+    # def retrieve(self, request, pk=None):
+    #     """Handle GET requests for single post
 
-        Returns:
-            Response -- JSON serialized game instance
-        """
-        try:
-            event = Event.objects.get(pk=pk)
-            serializer = EventSerializer(event, context={'request': request})
-            return Response(serializer.data)
-        except Exception:
-            return HttpResponseServerError(ex)
+    #     Returns:
+    #         Response -- JSON serialized game instance
+    #     """
+    #     try:
+    #         event = Event.objects.get(pk=pk)
+    #         serializer = EventSerializer(event, context={'request': request})
+    #         return Response(serializer.data)
+    #     except Exception:
+    #         return HttpResponseServerError(ex)
 
-    def update(self, request, pk=None):
-        """Handle PUT requests for an event
+    # def update(self, request, pk=None):
+    #     """Handle PUT requests for an event
 
-        Returns:
-            Response -- Empty body with 204 status code
-        """
-        scheduler = Gamer.objects.get(user=request.auth.user)
+    #     Returns:
+    #         Response -- Empty body with 204 status code
+    #     """
+    #     scheduler = Gamer.objects.get(user=request.auth.user)
 
-        event = Event.objects.get(pk=pk)
-        event.game = request.data["game"]
-        event.location = request.data["location"]
-        event.event_time = request.data["eventTime"]
-        event.scheduler = scheduler
+    #     event = Event.objects.get(pk=pk)
+    #     event.game = request.data["game"]
+    #     event.location = request.data["location"]
+    #     event.event_time = request.data["eventTime"]
+    #     event.scheduler = scheduler
 
-        game = Game.objects.get(pk=request.data["gameId"])
-        event.game = game
-        event.save()
+    #     game = Game.objects.get(pk=request.data["gameId"])
+    #     event.game = game
+    #     event.save()
 
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    #     return Response({}, status=status.HTTP_204_NO_CONTENT)
 
-    def destroy(self, request, pk=None):
-        """Handle DELETE requests for a single game
+    # def destroy(self, request, pk=None):
+    #     """Handle DELETE requests for a single game
 
-        Returns:
-            Response -- 200, 404, or 500 status code
-        """
-        try:
-            event = Event.objects.get(pk=pk)
-            event.delete()
+    #     Returns:
+    #         Response -- 200, 404, or 500 status code
+    #     """
+    #     try:
+    #         event = Event.objects.get(pk=pk)
+    #         event.delete()
 
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
+    #         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
-        except Event.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    #     except Event.DoesNotExist as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
-        except Exception as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    #     except Exception as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def list(self, request):
-        """Handle GET requests to events resource
+        """Handle GET requests to post resource
 
         Returns:
-            Response -- JSON serialized list of events
+            Response -- JSON serialized list of posts
         """
-        events = Event.objects.all()
+        posts = Post.objects.all()
 
-        # Support filtering events by game
-        game = self.request.query_params.get('gameId', None)
-        if game is not None:
-            events = events.filter(game__id=game)
-
-        serializer = EventSerializer(
-            events, many=True, context={'request': request})
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request})
         return Response(serializer.data)
