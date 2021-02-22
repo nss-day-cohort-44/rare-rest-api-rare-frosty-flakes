107b97489344a70ebe1e272707b056222af32e31
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-22 10:19:10 -0600

    adding Author comment

--- rareapi/models/post.py
+++ rareapi/models/post.py
@@ -1,8 +1,10 @@
+# AUTHOR: JARON LANE
+
 from django.db import models
 
 class Post(models.Model):
     user = models.ForeignKey("RareUser", on_delete=models.CASCADE)
-    category = models.ForeignKey("Category", verbose_name=_(""), on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     publication_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     models.ImageField(upload_to="photos", max_length=None)
