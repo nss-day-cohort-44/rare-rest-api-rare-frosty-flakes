a2e66d8af172e4b6b8d83b3aaa0ee3670f0a45a1
Danny McCracken <dannymccracken13@yahoo.com>
2021-02-23 14:02:18 -0600

    began creating fixture for comments

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -1,8 +1,3 @@
 from .rareuser import RareUser
-<<<<<<< HEAD
 from .category import Category
 from .comment import Comment
-=======
-from .tag import Tag
-from .category import Category
->>>>>>> main
